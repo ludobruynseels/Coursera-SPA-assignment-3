f1bc3985b77a9b0274791b7bdf11993a123bf4dd
Ludo <Ludo@ludobruynseels.com>
2017-02-01 22:50:27 +0100

    Filter the items

--- app.js
+++ app.js
@@ -10,15 +10,39 @@ angular.module('NarrowItDownApp', [])
        var NarrowIt = this;
        var promise =  MenuSearchService.GetAllItems();
 
+       //Initialize search string
+       NarrowIt.search = "";
+
+        // get menu items from the server
        promise.then ( function (response) {
                NarrowIt.Items = response.data.menu_items;
            }
        )
+
+        // handler for click event
+       NarrowIt.getMatchedMenuItems = function () {
+           console.log('you clicked!');
+
+           NarrowIt.found = NarrowIt.Items.filter(
+               function (item) {
+                   // if no search string entered, list all menu items.
+                   if (!NarrowIt.search)
+                       return true;
+
+                   var index = item.description.indexOf(NarrowIt.search);
+
+                   if (index === -1) return false;
+
+                   return true;
+               }
+           )
+       }
     }
+
     MenuSearchService.$inject = ['$http'];
     function MenuSearchService($http) {
        var service = this;
- /**/
+
        service.GetAllItems = function () {
            var response = $http(
                {
